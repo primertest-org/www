881947abd75b2e007214b13992c07cb8930ba073
primertest <32164253+primertest@users.noreply.github.com>
2017-10-17 14:33:59 +0100

    Update cloudshell.bash

--- cloudshell.bash
+++ cloudshell.bash
@@ -1,11 +1,13 @@
+git config --global user.email "primertest59@gmail.com" #your GitHub email address
+git config --global user.name "primertest" # your github username
 mkdir -p src
 cd src
-git clone https://github.com/primertest/www.git #the repo we want to clone/copy
-cd www #the directory that gets created by the git clone
+git clone https://github.com/mmistakes/jekyll-theme-basically-basic.git
+cd jekyll-theme-basically-basic
 rm -rf .git
 git init
-git remote add origin https://github.com/maxgoldhouse/mysite.git #the repo for your website files that we want to push to
+git remote add origin https://github.com/primertest/basic.git
 echo "first commit" >> README.md
 git add --all
-git commit -m 'The fun starts here'
+git commit -m 'The basic fun starts here'
 git push -f origin master
